1d7eda8c37192f7fcf182598f521c3d6645c4b38
Maddy Adams <maddy20adamsdev@icloud.com>
2019-12-13 15:32:05 -0500

    Initial Commit

---
+++ CustomRenderer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CustomRenderer
+//
+//  Created by Maddy Adams on 12/13/19.
+//  Copyright © 2019 Maddy Adams. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CustomRenderer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CustomRenderer
+//
+//  Created by Maddy Adams on 12/13/19.
+//  Copyright © 2019 Maddy Adams. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
